a07dde0a0093e834f3cc1750125c6fc1a761c2da
Ramashka Luba <lromashko911@gmail.com>
2021-08-25 00:58:39 +0300

    test changes 7

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ app.options("/*", function(req, res, next) {
 
 app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Request-Method', "*");
+    res.header('Access-Control-Allow-Methods', "*");
+    res.header('Access-Control-Allow-Headers', "*");
     next();
 });
 
