b040ad68f8b5bef6af989a70666369bdddb827f8
Ramashka Luba <lromashko911@gmail.com>
2021-08-25 00:37:17 +0300

    test changes 6

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.all('*', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, Authorization, Content-Length, X-Requested-With');
-    res.sendStatus(200);
+    //res.sendStatus(200);
     next();
 });
 
